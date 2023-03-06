de3f5a751d8a67c4087832d742b3b4e01cb0cd7c
daze573 <124431662+daze573@users.noreply.github.com>
2023-03-06 08:27:07 +0000

    [レイアウト前完了]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'homes/top'
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as:'about'
   devise_for :users
+  resources :users, only:[:show, :edit]
+  resources :books, only:[:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
