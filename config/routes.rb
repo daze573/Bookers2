e5e09a15638147c78734665c84278de56991118a
daze573 <124431662+daze573@users.noreply.github.com>
2023-03-06 10:09:17 +0000

    [ごはん休憩]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as:'about'
   devise_for :users
-  resources :users, only:[:show, :edit]
+  resources :users, only:[:index, :show, :edit]
   resources :books, only:[:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
