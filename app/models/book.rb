30889514111b907460ba0fe0d9834398745ab185
daze573 <124431662+daze573@users.noreply.github.com>
2023-03-11 10:01:55 +0000

    [夕飯]

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,8 @@ class Book < ApplicationRecord
   validates :body, presence: true
   # validates :image, presence:true
 
+  validates :body, length: { maximum: 200 }
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
